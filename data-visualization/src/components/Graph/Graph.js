6f77aa90386f174f414b4d83d2ccdb9203e19349
Dipali Makadia <dipali.makadia@gmail.com>
2024-03-24 13:19:50 -0700

    Updates:
    -- Implemented Highcharts.
    -- Build Bar and Pie charts.
    -- Displayed data into table.

--- data-visualization/src/components/Graph/Graph.js
+++ data-visualization/src/components/Graph/Graph.js
@@ -1,36 +1,219 @@
 import React, { useState, useEffect } from "react";
-// import JsonData from "../../API_DATA.json";
 import axios from "axios";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 const Graph = () => {
   const [graphData, setGraphData] = useState([]);
+  const tdp = Object.keys(graphData).map((val) =>
+    parseFloat(graphData[val]?.Performance?.TDP)
+  );
+  const cores = Object.keys(graphData).map((val) =>
+    parseFloat(graphData[val]?.Performance?.["# of Cores"])
+  );
+
+  let launched = 0;
+  let discontinued = 0;
+  let announced = 0;
+  for (const val in graphData) {
+    if (graphData[val]?.["Essentials"]?.["Status"] === "Launched") {
+      launched++;
+    } else if (graphData[val]?.["Essentials"]?.["Status"] === "Discontinued") {
+      discontinued++;
+    } else if (graphData[val]?.["Essentials"]?.["Status"] === "Announced") {
+      announced++;
+    }
+  }
+
+  let count32Bit = 0;
+  let count64Bit = 0;
+  Object.keys(graphData).map((val) => {
+    if (
+      graphData[val]?.["Advanced Technologies"]?.["Instruction Set"] ===
+      "32-bit"
+    ) {
+      count32Bit++;
+    } else if (
+      graphData[val]?.["Advanced Technologies"]?.["Instruction Set"] ===
+      "64-bit"
+    ) {
+      count64Bit++;
+    }
+  });
 
   useEffect(() => {
-    // Fetch method
-    // fetch(JsonData)
-    //   .then((response) => response.json())
-    //   .then((result) => setGraphData(result));
-    // console.log(JsonData)
-    //axios
-    axios.get(`/API_DATA.json`).then((result) => setGraphData(Object.values(result.data)));
-  });
+    axios
+      .get(`/API_DATA.json`)
+      .then((result) => setGraphData(Object.values(result.data)));
+  }, []);
+
+  const baroptions = {
+    chart: {
+      type: "column",
+    },
+    title: {
+      text: "Data:: # of Cores and TDP of every chip",
+    },
+    xAxis: {
+      categories: graphData.map((res) => res.name),
+    },
+    yAxis: {
+      max: 400,
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true,
+        },
+      },
+      series: {
+        pointPadding: 0,
+        groupPadding: 0.1,
+        borderWidth: 0,
+        shadow: false,
+      },
+    },
+    legend: {
+      borderWidth: 1,
+      backgroundColor: "#ffffff",
+      shadow: true,
+    },
+    credits: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        name: "TDP",
+        data: tdp,
+        tooltip: {
+          valueSuffix: " W",
+        },
+      },
+      {
+        name: "# of Cores",
+        data: cores,
+        tooltip: {
+          valueSuffix: "",
+        },
+      },
+    ],
+  };
+
+  const pieoptions = {
+    chart: {
+      type: "pie",
+    },
+    title: {
+      text: "Data:: No. of processors with Instruction Set of 64-bit and 32-bit",
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+        },
+      },
+      series: {
+        cursor: "pointer",
+        allowPointSelect: true,
+        borderWidth: 0,
+        shadow: false,
+        colorByPoint: true,
+        style: {
+          fontSize: "1.2em",
+          textOutline: "none",
+          opacity: 0.7,
+        },
+      },
+    },
+    legend: {
+      borderWidth: 1,
+      backgroundColor: "#ffffff",
+      shadow: true,
+    },
+    credits: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        name: "No. of Processors: ",
+        data: [
+          {
+            name: "32-bit",
+            y: count32Bit,
+          },
+          {
+            name: "64-bit",
+            y: count64Bit,
+          },
+        ],
+      },
+    ],
+  };
+
+  const pieeoptions = {
+    chart: {
+      type: "pie",
+    },
+    title: {
+      text: "Data:: No. of processors with Instruction Set of 64-bit and 32-bit",
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+        },
+      },
+      series: {
+        cursor: "pointer",
+        allowPointSelect: true,
+        borderWidth: 0,
+        shadow: false,
+        colorByPoint: true,
+        style: {
+          fontSize: "1.2em",
+          textOutline: "none",
+          opacity: 0.7,
+        },
+      },
+    },
+    legend: {
+      borderWidth: 1,
+      backgroundColor: "#ffffff",
+      shadow: true,
+    },
+    credits: {
+      enabled: false,
+    },
+
+    series: [
+      {
+        name: "No. of Processors: ",
+        data: [
+          {
+            name: "Announced",
+            y: announced,
+          },
+          {
+            name: "Launched",
+            y: launched,
+          },
+          {
+            name: "Discontinued",
+            y: discontinued,
+          },
+        ],
+      },
+    ],
+  };
+
   return (
-    <div>
-      {/* {graphData && <p>{JSON.stringify(graphData, null, 2)}</p>} */}
-      {graphData && graphData.map((res) => (
-        <div key={res.name}>
-            {res.Performance.TDP && <p>{res.Performance.TDP }</p>}
-            {/* {res['Performance'].map((per, val) =>(
-                <div key={per}>
-                    {val[Cache]}
-                </div>
-            ) )} */}
-        </div>
-      ))}
+    <div className="hello">
+      <HighchartsReact highcharts={Highcharts} options={baroptions} />
+      <HighchartsReact highcharts={Highcharts} options={pieoptions} />
+      <HighchartsReact highcharts={Highcharts} options={pieeoptions} />
     </div>
-    // <div>
-    //     {JSON.stringify(graphData[0])}
-    // </div>
   );
 };
 
--- data-visualization/src/components/Table/Table.js
+++ data-visualization/src/components/Table/Table.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const Table = () => {
-    return(
-        <div>Hello Table</div>
-    )
-}
+  const [tableData, setTableData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`/API_DATA.json`)
+      .then((result) => setTableData(Object.values(result.data)));
+  }, []);
+  console.log(tableData);
+  const uniqueKey = Object.values(tableData).reduce((key, val) => {
+    Object.keys(val).forEach((field) => {
+      if (!key.includes(field)) {
+        key.push(field);
+      }
+    });
+    return key;
+  }, []);
+  const subTable = (item, key) => {
+    if (typeof item[key] === 'object' && item[key] !== null) {
+      return (
+        <td key={key}>
+          <table>
+            <tbody>
+                {Object.entries(item[key]).map(([subKey, subVal]) => (
+                    <tr key={subKey}>
+                        <td>{subKey}</td>
+                        <td>{subVal}</td>
+                    </tr>
+                ))}
+            </tbody>
+          </table>
+        </td>
+      );
+    }else{
+        return <td key={key}>{item[key] || '--'}</td>
+    }
+  };
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            {uniqueKey.map((key, val) => (
+              <th key={val}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item, itemId) => (
+            <tr key={itemId}>
+                {uniqueKey.map((key, val) =>  subTable(item, key))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Table;
